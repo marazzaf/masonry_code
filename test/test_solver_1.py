Corrected sign of work of external forces. Now it finds a feasible solution. However, the resulting internal forces do not seem to make sense.

test/test_solver_1.py
@@ -32,20 +32,11 @@ for c1,c2 in GM.graph.edges:
         id_e = GM.graph[c1][c2]['id_edge'] - GM.Ne
         normal = GM.graph[c1][c2]['normal']
         stress_bnd[:,id_e] = -compression * normal
+        #plt.quiver(GM.graph[c1][c2]['bary'][0], GM.graph[c1][c2]['bary'][1], stress_bnd[0,id_e], stress_bnd[1,id_e], color='red') #For plot
+#plt.show()
 
 #print(stress_bnd)
 #sys.exit()
-
-##Creating a force on the boundary cells
-#compression = 1e2 #compressive force
-#force_bnd = np.zeros((d,len(GM.bnd)))
-#i = 0
-#for c in GM.bnd:
-#    GM.graph.nodes[c]['id_cell'] = i
-#    force_bnd[:,i] = compression * (GM.pos_bary - GM.graph.nodes[c]['pos']) #vector pointing towards the barycenter
-#    plt.quiver(GM.graph.nodes[c]['pos'][0], GM.graph.nodes[c]['pos'][1], force_bnd[0,i], force_bnd[1,i], color='red') #For plot
-#    i += 1
-#plt.show()
 
 #Assembling the system to minimize the energy
 E = Energy(GM, stress_bnd)
@@ -58,6 +49,7 @@ print(f)
 
 #Plotting the forces
 for c in GM.graph.nodes:
-    bary = GM.graph.nodes[c]['pos']
-    plt.quiver(bary[0], bary[1], f[c,0], f[c,1])
+    if c >= 0:
+        bary = GM.graph.nodes[c]['pos']
+        plt.quiver(bary[0], bary[1], f[c,0], f[c,1])
 plt.show()

utils/energy.py
@@ -22,8 +22,8 @@ class Energy:
             if G[c1][c2]['bnd']:
                 id_e = G[c1][c2]['id_edge'] - GM.Ne
                 for id_c,coord in zip(G[c1][c2]['bary_points'], G[c1][c2]['bary_coord']):
-                    c[d*id_c] += coord * G[c1][c2]['length'] * stress_bnd[0,id_e] #x component
-                    c[d*id_c+1] += coord * G[c1][c2]['length'] * stress_bnd[1,id_e] #y component
+                    c[d*id_c] -= coord * G[c1][c2]['length'] * stress_bnd[0,id_e] #x component
+                    c[d*id_c+1] -= coord * G[c1][c2]['length'] * stress_bnd[1,id_e] #y component
 
         #Energy for Tresca friction law
         s = GM.s_T #Tresca friction parameter
@@ -32,7 +32,7 @@ class Energy:
             if not G[c1][c2]['bnd']:
                 id_edge = G[c1][c2]['id_edge']
                 edge_matrix[id_edge] = G[c1][c2]['length']
-        c[d*Nc:] = s * edge_matrix
+        c[d*Nc:] = s * edge_matrix #Only involding cell dofs here
 
         return matrix(c, tc='d')
 
@@ -128,8 +128,8 @@ class Energy:
         aux1 = np.array(sol['z'][:Ne]).reshape(Ne) #Multiply by each edge normal to have the normal component of the force at each edge
         aux2 = sol['z'][Ne:]
         aux2 = np.array(aux2).reshape((Ne,d))
-        print(aux2)
-        sys.exit()
+        #print(aux2)
+        #sys.exit()
         aux2 = -aux2[:,0] + aux2[:,1] #Summing the components in each direction along t
         aux = np.array([aux1, aux2]).T #Force at each edge in (n,t) coordinates
         
@@ -137,10 +137,11 @@ class Energy:
         vec_forces = aux.copy()
         G =  GM.graph
         for c1,c2 in G.edges:
-            id_edge = G[c1][c2]['id_edge']
-            n = G[c1][c2]['normal']
-            t = G[c1][c2]['tangent']
-            vec_forces[id_edge,:] = vec_forces[0] * n + vec_forces[1] * t
+            if not G[c1][c2]['bnd']:
+                id_edge = G[c1][c2]['id_edge']
+                n = G[c1][c2]['normal']
+                t = G[c1][c2]['tangent']
+                vec_forces[id_edge,:] = vec_forces[0] * n + vec_forces[1] * t
         return vec_forces #Forces in (e_1,e_2) basis
 
         ##Returning forces in each cell