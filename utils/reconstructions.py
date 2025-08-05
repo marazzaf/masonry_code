2013f9422825ac6f8dfa3fd2ebc3d65e6c1a056d
Frederic Marazzato <marazzato@arizona.edu>
2025-08-05 09:35:57 -0700

    Was able to get the id for the normal stresses and the facet to be marked. Check that the stress bc are taken into account correctly.

--- utils/reconstructions.py
+++ utils/reconstructions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from firedrake.petsc import PETSc
+from firedrake import *
 import matplotlib.pyplot as plt
 import sys
 from graph import *
@@ -45,42 +46,53 @@ def stress_reconstruction(GM, stress_bnd, normal_stresses):
     res = []
     G =  GM.graph
     for c1 in G.nodes:
-        if c1 >= 0: #Actual cell. Note boundary facet.
+        if c1 >= 0: #Actual cell. Not boundary facet.
             #Create the Mesh with DMPlex
             plex = create_plex(GM, c1)
-            sys.exit()
+
+            #print(plex.getDepthStratum(1)) #edges
 
             #Creating a list of markers and Dirichlet BC
             bnd_condition = [] #Value of normal stress on bnd
             bnd_marker = [] #Marker for the bnd
 
-            boundary_facets = plex.getStratumIS("bnd", GM.d-1).getIndices()
-            # Sort by order they appear
-            for idx, facet in enumerate(boundary_facets):
-                plex.setLabelValue('bnd', facet, idx) #Marking bnd
-
-            
-            for c2 in G.neighbors(c1):
+            #Looping through facets of the cell
+            for f in GM.voronoi[c1]['faces']:
+                c2 = f['adjacent_cell']
                 if not G[c1][c2]['bnd']:
-                    id_e = G[c1][c2]['id_edge']
+                    id_e = G[c1][c2]['id_edge'] #Id of the edge
                     
                     #Compute outward unit normal
                     n = G.nodes[c2]['pos'] - G.nodes[c1]['pos']
                     n /= np.linalg.norm(n) #unit normal
 
                     #Compute BC
-                    bc = np.dot(normal_stresses[:, id_e], n)
-                    bnd_condition.append(bc)
+                    bc = np.outer(normal_stresses[:, id_e], n)
+                    
 
-                    #Marker
-                    #Mark the correct bnd edge
                 else: #boundary facet
                     n = G[c1][c2]['normal']
-                    id_e -= GM.Ne
-                    force = stress_bnd[:, id_e] * G[c1][c2]['length']
-                    #force = np.zeros_like(force) #test
-                    #force = -n * G[c1][c2]['length'] #test
+                    id_e -= GM.Ne #Id of the edge
 
+                    #Compute BC
+                    bc = np.outer(stress_bnd[:, id_e], n)
+
+                #Store BC
+                bnd_condition.append(bc)
+
+                #Find the edge in the plex
+                cell_start, cell_end = plex.getDepthStratum(2)
+                v1,v2 = list(np.array(f['vertices']) + cell_end)
+                edge = plex.getJoin([v1, v2])
+                #print(v1,v2,edge)
+                
+                
+                #Mark the edge in the plex
+                plex.setLabelValue('bnd', edge, id_e) #Marking bnd
+                bnd_marker.append(id_e)
+                
+            #sys.exit()
+            
             #Solving the system for the stress reconstruction
             stress = reconstruct_stress_polygon(plex, bnd_condition, bnd_marker)
 
@@ -92,36 +104,30 @@ def stress_reconstruction(GM, stress_bnd, normal_stresses):
 def create_plex(GM, cell_index):
     #Vertices of the mesh
     vertices = GM.voronoi[cell_index]['vertices'] #Vertices from Voronoi cell
-    vertices.append(GM.voronoi[cell_index]['original']) #Adding vornoi center as last element
+    vertices.append(GM.voronoi[cell_index]['original']) #Adding voronoi center as last element
 
     #Cells of the mesh
     cells = []
     for f in GM.voronoi[cell_index]['faces']:
         cells.append(f['vertices'] + [len(vertices)-1])
 
-    # === Create DMPlex mesh with topology ===
-    plex = PETSc.DMPlex().createFromCellList(GM.d, cells, vertices, interpolate=True)   
+    #Create DMPlex mesh with vertices and ells
+    plex = PETSc.DMPlex().createFromCellList(GM.d, cells, vertices, interpolate=True)
 
     #Mark boundary facets
     plex.markBoundaryFaces("bnd")
 
-    #Test
-    print(GM.voronoi[cell_index]['faces'])
-    edge_start, edge_end = plex.getDepthStratum(1)
-    for edge in range(edge_start, edge_end):
-        cone = plex.getCone(edge)
-        print(f"Edge {edge} connects vertices {cone[:]}")
-
     return plex
 
-def edge_normal(v0, v1):
-    t = v1 - v0
-    n = np.array([-t[1], t[0]])
-    return n / np.linalg.norm(n)
+#def edge_normal(v0, v1):
+#    t = v1 - v0
+#    n = np.array([-t[1], t[0]])
+#    return n / np.linalg.norm(n)
 
 def reconstruct_stress_polygon(plex, bnd_condition, bnd_marker):
     #Create mesh
     mesh = Mesh(plex)
+    
     #Mixed FEM space
     V = VectorFunctionSpace(mesh, 'BDM', 1)
     W = FunctionSpace(mesh, 'DG', 0)
@@ -140,7 +146,10 @@ def reconstruct_stress_polygon(plex, bnd_condition, bnd_marker):
     L = Constant(0) * q * dx
 
     #Dirichlet BC
-    bcs = [] #Write that
+    bcs = []
+    for mark,bc in zip(bnd_marker,bnd_condition):
+        bc = DirichletBC(Z.sub(0), Constant(bc), mark)
+        bcs.append(bc)
 
     #Solving
     res = Function(Z)
