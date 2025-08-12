6c15a6234f06d5cf0f771a22cff15057f3ff649a
Frederic Marazzato <marazzato@arizona.edu>
2025-08-12 14:51:33 -0700

    Updating computation with a random Voronoi mesh. Try to impose a non-homogeneous compression?

--- computations/comp.py
+++ computations/comp.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-sys.path.append('./utils')
+sys.path.append('../utils/')
 from graph import *
 from energy import *
 import matplotlib.pyplot as plt
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 np.random.seed(seed=136985)
 
 #Material parameter for friction
-s = 10
+s = 1
 
 #Getting the points
 d = 2 #Space dimension
@@ -17,53 +17,33 @@ N = 60 #20
 pts = np.random.uniform(size=d*N)
 points = pts.reshape((N,d))
 
+#Creating the graph
 GM = GranularMaterial(points, d, s)
 
 ##Plotting points
 #GM.plot_graph()
 #GM.plot_voronoi()
+#sys.exit()
 
-#Creating a force on the boundary cells
-compression = 5e1 #compressive force
-force_bnd = np.zeros((d,len(GM.bnd)))
-i = 0
-##Anisotropic force
-#for c in GM.bnd:
-#    GM.graph.nodes[c]['id_cell'] = i
-#    pos = GM.graph.nodes[c]['pos']
-#    if pos[0] < .2:
-#        force_bnd[:,i] = np.array([1, 0])
-#    elif pos[0] > .8:
-#        force_bnd[:,i] = -np.array([1, 0])
-#    if pos[1] < .2:
-#        force_bnd[:,i] += np.array([0, 1])
-#    elif pos[1] > .8:
-#        force_bnd[:,i] += compression * np.array([0, -1])
-#    plt.quiver(GM.graph.nodes[c]['pos'][0], GM.graph.nodes[c]['pos'][1], force_bnd[0,i], force_bnd[1,i], color='red') #For plot
-#    i += 1
-#plt.show()
-
-#Isotropic force
-for c in GM.bnd:
-    GM.graph.nodes[c]['id_cell'] = i
-    force_bnd[:,i] = compression * (GM.pos_bary - GM.graph.nodes[c]['pos']) #vector pointing towards the barycenter
-    plt.quiver(GM.graph.nodes[c]['pos'][0], GM.graph.nodes[c]['pos'][1], force_bnd[0,i], force_bnd[1,i], color='red') #For plot
-    i += 1
+#Neumann condition on boundary edges
+compression = 1 #1e2 #compressive force
+eps = 1
+S = -compression * np.array([[1, eps], [eps,1]])
+stress_bnd = np.zeros((d, GM.Nbe))
+for c1,c2 in GM.graph.edges:
+    if GM.graph[c1][c2]['bnd']:
+        id_e = GM.graph[c1][c2]['id_edge'] - GM.Ne
+        normal = GM.graph[c1][c2]['normal']
+        stress_bnd[:,id_e] = np.dot(S, normal)
 
 #Assembling the system to minimize the energy
-E = Energy(GM, force_bnd)
+E = Energy(GM, stress_bnd)
 
-#Computing the forces
+#Computing the normal stresses
 f = E.solve(GM)
-print(f)
-sys.exit()
 
-#Test stress reconstruction
-GM.stress_reconstruction(f)
-
-#Plotting the forces
-for c in GM.graph.nodes:
-    bary = GM.graph.nodes[c]['pos']
-    if np.linalg.norm(f[c,:]) > 1e-5:
-        plt.quiver(bary[0], bary[1], f[c,0], f[c,1])
-plt.show()
+#Stress reconstruction
+stress = stress_reconstruction(GM, stress_bnd, f)
+file = VTKFile('test.pvd')
+for (i,s) in enumerate(stress):
+    file.write(s,idx=i)
